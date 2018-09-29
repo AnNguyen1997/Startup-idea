4bcbf24fd4697279284ec42962f2f06d7fed391f
Hai Truong <ximbong91023@yahoo.com>
2018-09-29 17:10:59 +0300

    Host react app in production

--- server.js
+++ server.js
@@ -17,6 +17,15 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
