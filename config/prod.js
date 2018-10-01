3613d2364ff4d1362905710a83703372f387d3d5
Hai Truong <ximbong91023@yahoo.com>
2018-10-01 08:56:59 +0300

    Add database connection

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    db_id: process.env.db_id,
+    db_pw: process.env.db_pw,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
+    secret: process.env.secret
+  };
