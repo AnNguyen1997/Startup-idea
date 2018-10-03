197f641a9cc477781f13af1a229fb83bb5617f47
Hai Truong <ximbong91023@yahoo.com>
2018-10-04 00:01:56 +0300

    Fix upload route

--- routes/upload.js
+++ routes/upload.js
@@ -17,11 +17,14 @@ const upload = multer({ dest: "../tmp/uploads" });
 router.post("/", upload.single("image"), function(req, res, next) {
   const { _id, username } = req.user;
 
+  console.log('req.user', req.user);
+  console.log('req.file', req.file);
+
   cloudinary.v2.uploader.upload(req.file.path, function(error, result) {
     const image_url = result.secure_url;
     User.findById(_id, function(err, user) {
       if (err) return console.error(err);
-      user.image_url.push(post_id);
+      user.image_url.push(image_url);
       user.save(function(err, post) {
         if (err) return console.error(err);
       });
