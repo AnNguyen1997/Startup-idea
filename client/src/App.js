8a7e79177fa12f6e9b9cd86e1b65bbe44556897c
Hai Truong <ximbong91023@yahoo.com>
2018-10-03 22:04:12 +0300

    Render components on App

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,80 @@
-import React, { Component } from 'react';
-import './App.css';
-import DrawingPanel from './components/DrawingPanel';
+import React, { Component } from "react";
+import DrawingPanel from "./components/DrawingPanel";
+import Loader from "./components/Loader";
+import LoginForm from "./components/LoginForm";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
+
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loader: true,
+      username: null
+    };
+  }
+
+  componentDidMount = () => {
+    this.checkAuth();
+  };
+
+  handleLogIn = username => {
+    this.setState({
+      username
+    });
+  };
+
+  handleLogOut = () => {
+    fetch("/logout")
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({
+          username: res.username
+        });
+      });
+  };
+
+  checkAuth = () => {
+    fetch("/auth")
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+        setTimeout(() => {
+          this.setState({
+            username: res.username,
+            loader: false
+          });
+        }, 2000);
+      });
+  };
+
   render() {
-    return (
-      <DrawingPanel />
+    const { loader, username } = this.state;
+
+    const Routes = (
+      <Router>
+        <React.Fragment>
+          <Switch>
+            <Route
+              path="/login"
+              render={() => <LoginForm handleLogIn={this.handleLogIn} />}
+            />
+            {!username && !loader && <Redirect to="/login" />}
+          </Switch>
+          <Route path="/" exact component={DrawingPanel} />
+        </React.Fragment>
+      </Router>
     );
+
+    return loader ? <Loader /> : Routes;
   }
 }
 
